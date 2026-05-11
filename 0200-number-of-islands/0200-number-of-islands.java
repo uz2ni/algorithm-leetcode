86d9d2e4de84fca48978eb8091c90115ef48f404
uz2ni <uzini_@likelion.org>
2026-05-11 11:46:35 +0900

    Time: 5 ms (27.83%), Space: 51.2 MB (95.55%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -3,7 +3,6 @@ class Solution {
         int answer = 0;
         int r = grid.length;
         int c = grid[0].length;
-        System.out.println("r:" + r + ", c:" + c);
         boolean[][] visited = new boolean[r][c];
         for(int i=0; i<r; i++) {
             for(int j=0; j<c; j++) {
@@ -20,26 +19,24 @@ class Solution {
     public void bfs(char[][] grid, int i, int j, int r, int c, boolean[][] visited) {
         Queue<int[]> q = new ArrayDeque<>();
         q.offer(new int[]{i,j});
+        visited[i][j] = true;
 
         // 상하좌우
-        int[] dx = {0,0,-1,1};
-        int[] dy = {-1,1,0,0};
+        int[] dc = {0,0,-1,1};
+        int[] dr = {-1,1,0,0};
         
         while(!q.isEmpty()) {
             int[] cur = q.poll();
-            // 이미 방문했으면 continue
-            if(visited[cur[0]][cur[1]]) continue;
-
-            visited[cur[0]][cur[1]] = true;
 
             // 상하좌우에 방문안했고, 1이면 큐 삽입
             for(int k=0; k<4; k++) {
-                int nx = cur[1]+dx[k];
-                int ny = cur[0]+dy[k];
+                int nc = cur[1]+dc[k];
+                int nr = cur[0]+dr[k];
 
-                if((0 <= nx) && (nx < c) && (0 <= ny) && (ny < r)) {
-                    if(!visited[ny][nx] && (grid[ny][nx]=='1')) {
-                        q.offer(new int[]{ny,nx});
+                if((0 <= nc) && (nc < c) && (0 <= nr) && (nr < r)) {
+                    if(!visited[nr][nc] && (grid[nr][nc]=='1')) {
+                        q.offer(new int[]{nr,nc});
+                        visited[nr][nc] = true;
                     }
                 }
             }
