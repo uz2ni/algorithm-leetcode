c0a6ef294debe20e0326c15ce5370c958cc07f13
uz2ni <uzini_@likelion.org>
2026-07-13 15:25:20 +0900

    Time: 15 ms (20.67%), Space: 46.1 MB (34.53%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,20 +1,45 @@
 class Solution {
-    public boolean isPalindrome(String s) {
-        s = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+    public boolean isPalindrome2(String s) {
+        // 영숫자 이외 문자 제거, 앞뒤 읽었을 때 내용 동일해야 함
+        
+        // 1. 소문자 변환, 비대상 문자 제거
+        String cleanS = s.toLowerCase().replaceAll("[^a-z0-9]", "");
 
+        // 2. String to char[]
+        char[] chs = cleanS.toCharArray();
+
+        // 3. 내용 동일 체크
         int left = 0;
-        int right = s.length()-1;
-
+        int right = chs.length-1;
         while(left < right) {
-            
-            if(s.charAt(left) != s.charAt(right)) {
+            if(chs[left] != chs[right]) {
                 return false;
             }
-
             left++;
             right--;
         }
 
         return true;
     }
+
+    // [개선] toCharArray() 없이 String 에서 바로 비교
+    public boolean isPalindrome(String s) {
+        // 영숫자 이외 문자 제거, 앞뒤 읽었을 때 내용 동일해야 함
+        
+        // 1. 소문자 변환, 비대상 문자 제거
+        String cleanS = s.toLowerCase().replaceAll("[^a-z0-9]", "");
+
+        // 3. 내용 동일 체크
+        int left = 0;
+        int right = cleanS.length()-1;
+        while(left < right) {
+            if(cleanS.charAt(left) != cleanS.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+
+        return true;
+    }    
 }
