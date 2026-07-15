09a41dd9ab7f430d60e16e095e6f9d68c242c9e6
uz2ni <uzini_@likelion.org>
2026-07-15 13:34:40 +0900

    Time: 4 ms (46.47%), Space: 52.2 MB (68.48%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,46 +1,29 @@
 class Solution {
+    public int cnt;
+    public int[] dx = {-1,1,0,0};
+    public int[] dy = {0,0,-1,1};
     public int numIslands(char[][] grid) {
-        int answer = 0;
-        int r = grid.length;
-        int c = grid[0].length;
-        boolean[][] visited = new boolean[r][c];
-        for(int i=0; i<r; i++) {
-            for(int j=0; j<c; j++) {
-                if(!visited[i][j] && (grid[i][j]=='1')) {
-                    bfs(grid, i, j, r, c, visited);
-                    answer++;
-                }
+        cnt = 0;
+        boolean[][] visited = new boolean[grid.length][grid[0].length];
+        for(int i=0; i<grid.length; i++) {
+            for(int j=0; j<grid[0].length; j++) {
+                if(visited[i][j] || (grid[i][j] == '0')) continue;
+                dfs(grid, visited, grid.length, grid[0].length, i, j);
+                cnt++;    
             }
         }
-        
-        return answer;
+        return cnt;
     }
 
-    public void bfs(char[][] grid, int i, int j, int r, int c, boolean[][] visited) {
-        Queue<int[]> q = new ArrayDeque<>();
-        q.offer(new int[]{i,j});
-        visited[i][j] = true;
+    public void dfs(char[][] grid, boolean[][] visited, int m, int n, int x, int y) {
+        visited[x][y] = true;
 
-        // 상하좌우
-        int[] dc = {0,0,-1,1};
-        int[] dr = {-1,1,0,0};
-        
-        while(!q.isEmpty()) {
-            int[] cur = q.poll();
-
-            // 상하좌우에 방문안했고, 1이면 큐 삽입
-            for(int k=0; k<4; k++) {
-                int nc = cur[1]+dc[k];
-                int nr = cur[0]+dr[k];
-
-                if((0 <= nc) && (nc < c) && (0 <= nr) && (nr < r)) {
-                    if(!visited[nr][nc] && (grid[nr][nc]=='1')) {
-                        q.offer(new int[]{nr,nc});
-                        visited[nr][nc] = true;
-                    }
-                }
-            }
+        for(int i=0; i<4; i++) {
+            int nx = dx[i]+x;
+            int ny = dy[i]+y;
+            if(nx < 0 || ny < 0 || nx >= m || ny >= n) continue;
+            if(visited[nx][ny] || (grid[nx][ny] == '0')) continue;
+            dfs(grid, visited, m, n, nx, ny);
         }
-        
     }
 }
