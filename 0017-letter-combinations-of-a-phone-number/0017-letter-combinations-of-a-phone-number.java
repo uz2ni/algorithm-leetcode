dcf07ec5997b4a416e7a120bf39e33bf563529b5
uz2ni <uzini_@likelion.org>
2026-07-15 15:15:02 +0900

    Time: 3 ms (32.18%), Space: 48.8 MB (25.31%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,10 +1,13 @@
 class Solution {
-    public static List<String> answerList;
+    public List<String> answerList;
     public List<String> letterCombinations(String digits) {
+
+        if(digits.length()==0) return new ArrayList<>();
+
         answerList = new ArrayList<>();
 
-        char[][] chars = {
-                            {' '},
+        char[][] letters = {
+                            {},
                             {},
                             {'a','b','c'},
                             {'d','e','f'},
@@ -18,12 +21,12 @@ class Solution {
         
         char[] nums = digits.toCharArray();
 
-        recursive(nums, chars, 0, "");
+        recursive(nums, letters, 0, "");
 
         return answerList;
     }
 
-    public static void recursive(char[] nums, char[][] chars, int depth, String str) {
+    public void recursive(char[] nums, char[][] letters, int depth, String str) {
 
         if(nums.length == depth) {
             answerList.add(str);
@@ -32,8 +35,8 @@ class Solution {
 
         int n = nums[depth] - '0';
 
-        for(char c : chars[n]) {
-            recursive(nums, chars, depth+1, str+c);
+        for(char c : letters[n]) {
+            recursive(nums, letters, depth+1, str+c);
         }
     }
 }
