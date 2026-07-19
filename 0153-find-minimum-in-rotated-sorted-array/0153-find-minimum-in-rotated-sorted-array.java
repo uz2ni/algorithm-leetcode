738b5fc59ef39bc982d28b5837a01f4ba516bf5b
uz2ni <uzini_@likelion.org>
2026-07-19 16:35:46 +0900

    Time: 0 ms (100%), Space: 43.5 MB (94.93%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.java
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.java
@@ -1,5 +1,7 @@
 class Solution {
-    public int findMin(int[] nums) {
+    // 1. O(N) 이라 의도와 다름
+    // 최악의 경우 전체 탐색이라 중간값 두는 의미가 없음
+    public int findMin1(int[] nums) {
 
         if(nums.length == 1) return nums[0];
 
@@ -20,5 +22,24 @@ class Solution {
         }
         
         return nums[0];
+    }
+
+    // 2. O(LogN), 중앙값 두고 분할하면서 정답 찾기
+    public int findMin(int[] nums) {
+        if(nums.length == 1) return nums[0];
+
+        int left = 0;
+        int right = nums.length-1;
+        
+        while(left < right) {
+            int mid = (right-left)/2 + left;
+            if(nums[mid] < nums[right]) {
+                right = mid;
+            }else {
+                left = mid+1;
+            }
+        }
+
+        return nums[left]; // nums[right] 도 가능
     }
 }
