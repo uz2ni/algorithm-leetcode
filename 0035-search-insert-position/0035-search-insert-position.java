bc2f39905daff018d1ede13daebf984f584e38c6
uz2ni <uzini_@likelion.org>
2026-07-19 17:13:48 +0900

    Time: 0 ms (100%), Space: 44.5 MB (83.36%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int searchInsert(int[] nums, int target) {
+    public int searchInsert1(int[] nums, int target) {
         int left = 0;
         int right = nums.length-1;
 
@@ -9,11 +9,27 @@ class Solution {
                 left = mid+1;
             }else if(nums[mid] > target) {
                 right = mid;
-            }else if(nums[mid] == target){
+            }else{
                 return mid;
             }
         }
         
         return (nums[left] < target) ? left+1 : left;
+    }
+
+    public int searchInsert(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length-1;
+
+        while(left <= right) {
+            int mid = (right-left)/2 + left;
+            if(nums[mid] < target) {
+                left = mid+1;
+            }else {
+                right = mid-1;
+            }
+        }
+        
+        return left;
     }
 }
