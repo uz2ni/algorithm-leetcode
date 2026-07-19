88e2c7bca29f083ec02a25028be3e488447d3d5c
uz2ni <uzini_@likelion.org>
2026-07-19 15:41:17 +0900

    Time: 0 ms (100%), Space: 43.4 MB (84.94%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -15,7 +15,7 @@
  */
 class Solution {
     // 1. bfs
-    public List<Integer> rightSideView(TreeNode root) {
+    public List<Integer> rightSideView1(TreeNode root) {
         
         if(root == null) return new ArrayList<>();
 
@@ -36,5 +36,24 @@ class Solution {
         }
 
         return answers;
+    }
+
+    // 2. dfs
+    public List<Integer> answers;
+    public List<Integer> rightSideView(TreeNode root) {
+        answers = new ArrayList<>();
+        dfs(root, 0);
+        return answers;
+    }
+
+    public void dfs(TreeNode start, int depth) {
+        if(start == null) return;
+        
+        if(answers.size() == depth) {
+            answers.add(start.val);
+        }
+
+        if(start.right != null) dfs(start.right, depth+1);
+        if(start.left != null) dfs(start.left, depth+1);
     }
 }
