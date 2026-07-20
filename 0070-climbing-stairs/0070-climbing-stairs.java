e1e9f52519378d05d4649f524aa94c986f1adff3
uz2ni <uzini_@likelion.org>
2026-07-20 15:49:22 +0900

    Time: 0 ms (100%), Space: 42.1 MB (36.1%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -5,9 +5,11 @@ class Solution {
         // dfs1(n, 0);
         // System.out.println(cnt);
 
-        int[] memo = new int[n+1];
-        Arrays.fill(memo, -1);
-        cnt = dp(n, 0, memo);
+        // int[] memo = new int[n+1];
+        // Arrays.fill(memo, -1);
+        // cnt = dp1(n, 0, memo);
+
+        cnt = dp(n);
         return cnt;
     }
 
@@ -25,13 +27,29 @@ class Solution {
         dfs(n, sum+2);
     }
 
-    public int dp(int n, int sum, int[] memo) {
+    // top-down (큰문제->작은문제, 재귀+memoization)
+    public int dp1(int n, int sum, int[] memo) {
         if(n == sum) return 1; // 현재까지 왔을 때 경우의수 1개 찾은 것 리턴
         if(n < sum) return 0; // 현재 경로는 해답될 수 없음
 
         if(memo[sum] != -1) return memo[sum];
 
-        memo[sum] = dp(n, sum+1, memo) + dp(n, sum+2, memo);
+        memo[sum] = dp1(n, sum+1, memo) + dp1(n, sum+2, memo);
         return memo[sum];
-    }    
+    } 
+
+    // bottom-up (작은문제->큰문제, dp table+for)
+    public int dp(int n) {
+        if(n <= 2) return n;
+
+        int[] dp = new int[n+1];
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for(int i=3; i<=n; i++) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+
+        return dp[n];
+    }
 }
