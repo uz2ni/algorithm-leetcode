19854905312037c26e323c74e6639308320e5d45
uz2ni <uzini_@likelion.org>
2026-07-26 01:20:58 +0900

    Time: 1 ms (86.74%), Space: 44.4 MB (20.48%) - LeetHub

---
+++ 0078-subsets/0078-subsets.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<List<Integer>> answers;
+    public List<List<Integer>> subsets(int[] nums) {
+        // 부분 집합 모두 구하기
+        answers = new ArrayList<>();
+        dfs(nums, 0, new ArrayList<>());   
+        return answers;
+    }
+
+    public void dfs(int[] nums, int start, List<Integer> list) {
+        answers.add(new ArrayList<>(list));
+
+        if(nums.length == start) return;
+
+        for(int i=start; i<nums.length; i++) {
+            list.add(nums[i]);
+            dfs(nums, i+1, list);
+            list.remove(list.size()-1);
+        }
+    }
+}
