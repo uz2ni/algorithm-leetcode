bbb6e643c1419c96c3c1cd90d9e51c484ece3cf1
uz2ni <uzini_@likelion.org>
2026-07-27 13:59:46 +0900

    Time: 0 ms (100%), Space: 61.2 MB (71.3%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,5 +1,5 @@
 class Solution {
-    // 1. 브루트포스 O(NxK) : 시간 초과
+    // 1. 브루트포스. 시간 O(NxK), 공간 O(1) - 시간 초과
     public void rotate1(int[] nums, int k) {
         int temp = 0;
         k = k % nums.length; // k가 nums보다 크면 여러 바퀴 도는 것을 생략하기 위함
@@ -12,16 +12,36 @@ class Solution {
         }
     }
 
-    // 2. 계산한 각 위치로 이동. 공간복잡도 O(N)
-    public void rotate(int[] nums, int k) {
+    // 2. 계산한 각 위치로 이동. 시간 O(N), 공간 O(N) - 통과
+    public void rotate2(int[] nums, int k) {
         int[] answers = new int[nums.length];
         for(int i=0; i<nums.length; i++) {
             int idx = (i+k) % nums.length;
             answers[idx] = nums[i];
         }
         // 복사
-        for(int i=0; i<nums.length; i++) {
-            nums[i] = answers[i];
+        System.arraycopy(answers,0,nums,0,nums.length);
+    }
+
+    // 3. 뒤집기. 투포인터 활용. 시간
+    public void rotate(int[] nums, int k) {
+        k = k % nums.length;
+
+        // 1) 전체 뒤집기
+        reverse(nums, 0, nums.length-1);
+        // 2) 0~k-1 뒤집기
+        reverse(nums, 0, k-1);
+        // 3) k~length-1 뒤집기
+        reverse(nums, k, nums.length-1);
+    }
+
+    public void reverse(int[] nums, int start, int end) {
+        while(start<end) {
+            int temp = nums[end];
+            nums[end] = nums[start];
+            nums[start] = temp;
+            start++;
+            end--;
         }
     }
 }
