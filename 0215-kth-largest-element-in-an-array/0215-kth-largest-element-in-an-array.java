f730c2aec66bf81261d8634535202c13ee0a8005
uz2ni <uzini_@likelion.org>
2026-07-29 15:05:50 +0900

    Time: 70 ms (44.82%), Space: 74.6 MB (33.15%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -1,27 +1,13 @@
 class Solution {
-    // 1. 정렬 후 구하기
-    public int findKthLargest1(int[] nums, int k) {
-        Arrays.sort(nums);
-        for(int i=nums.length-1; i>=0; i--) {
-            if(i == nums.length-k) {
-                System.out.println(nums[i]);
-                return nums[i];
-            }
-        }
-        return -1;
-    }
-    // 2. 최대힙(우선순위큐)으로 삽입 후 k번 뺀 값
+    // 최소힙 : O(NLogK)
     public int findKthLargest(int[] nums, int k) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
-        for(int n : nums) {
-            pq.offer(n);
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        
+        for(int i=0; i<nums.length; i++) {
+            pq.offer(nums[i]);
+            if(pq.size() > k) pq.poll(); // 큰 수 k개만 남도록 최소값 빼기
         }
-        for(int i=1; i<=k; i++) {
-            if(i == k) {
-                return pq.poll();
-            }
-            pq.poll();
-        }
-        return -1;
+
+        return pq.poll();
     }
 }
