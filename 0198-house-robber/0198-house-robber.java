5f9a18bf6f42de56122f9e0c9281b547a534a67c
uz2ni <uzini_@likelion.org>
2026-08-02 16:56:13 +0900

    Time: 0 ms (100%), Space: 42.9 MB (38.03%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,39 +1,24 @@
 class Solution {
     public int rob(int[] nums) {
-        // int[] memo = new int[nums.length];
-        // Arrays.fill(memo, -1);
-        // return topDown(nums, 0, memo);
+        int n = nums.length-1;
 
-        return bottomUp(nums);
-    }
-
-    // 1. topdown : idx~마지막 집까지 털 수 있는 최대 금액
-    public int topDown(int[] nums, int idx, int[] memo) {
-        // 더이상 집이 없는 경우
-        if(idx >= nums.length) return 0;
-
-        // 이미 계산한 경우
-        if(memo[idx] != -1) return memo[idx];
-        
-        // 계산 안되어있을 때 다음 구간 계산
-        int rob = nums[idx]+topDown(nums, idx+2, memo); // 현재 털 경우
-        int skip = topDown(nums, idx+1, memo); // 현재 안털 경우
-        memo[idx] = Math.max(rob, skip);
-        return memo[idx];
-    }
-
-    // 1. bottom-up
-    public int bottomUp(int[] nums) {
-        if(nums.length == 1) return nums[0];
-
-        int[] dp = new int[nums.length];
-        dp[0] = nums[0];
-        dp[1] = Math.max(nums[0], nums[1]);
-
-        for(int i=2; i<nums.length; i++) {
-            dp[i] = Math.max(dp[i-1], dp[i-2]+nums[i]);
+        if(n == 0) return nums[0];
+        if(n == 1) {
+            return Math.max(nums[0],nums[1]);
+        }
+        if(n == 2) {
+            return Math.max(nums[0]+nums[2],nums[1]);
         }
         
-        return dp[nums.length-1];
+        int[] dp = new int[n+1];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+        dp[2] = Math.max(nums[0]+nums[2], nums[1]);
+        
+        for(int i=3; i<=n; i++) {
+            dp[i] = Math.max(dp[i-2],dp[i-3])+nums[i];
+        }
+
+        return Math.max(dp[n], dp[n-1]);
     }
 }
