421afa3d5609bd0a4cd1c7cedec439f9fce95863
uz2ni <uzini_@likelion.org>
2026-08-02 16:21:38 +0900

    Time: 0 ms (100%), Space: 42.2 MB (17.75%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,55 +1,41 @@
 class Solution {
-    public int cnt;
+    // bottom-up
+    public int climbStairs1(int n) {
+        // 1칸 or 2칸씩 계단 올라갈 수 있음
+        // 꼭대기까지 올라갈 수 있는 경우의 수
+        if(n == 1) return 1;
+        if(n == 2) return 2;
+        
+        int[] tb = new int[n+1];
+        Arrays.fill(tb, -1);
+        
+        tb[1] = 1;
+        tb[2] = 2;
+
+        for(int i=3; i<=n ;i++) {
+            tb[i] = tb[i-2] + tb[i-1];
+        }
+
+        return tb[n];
+    }
+
+    // top-down
     public int climbStairs(int n) {
-        cnt = 0;
-        // dfs1(n, 0);
-        // System.out.println(cnt);
+        int[] m = new int[n+1];
+        Arrays.fill(m, -1);
 
-        // int[] memo = new int[n+1];
-        // Arrays.fill(memo, -1);
-        // cnt = dp1(n, 0, memo);
-
-        cnt = dp(n);
-        return cnt;
+        return dfs(n, m);
     }
 
-    // 시간초과
-    public void dfs(int n, int sum) {
-        // basecase
-        if(n == sum) {
-            cnt++;
-            return;
-        }
-        if(n < sum) {
-            return;
-        }
-        dfs(n, sum+1);
-        dfs(n, sum+2);
-    }
+    public int dfs(int n, int[] m) {
+        if(n == 1) return 1;
+        if(n == 2) return 2;
 
-    // top-down (큰문제->작은문제, 재귀+memoization)
-    public int dp1(int n, int sum, int[] memo) {
-        if(n == sum) return 1; // 현재까지 왔을 때 경우의수 1개 찾은 것 리턴
-        if(n < sum) return 0; // 현재 경로는 해답될 수 없음
-
-        if(memo[sum] != -1) return memo[sum];
-
-        memo[sum] = dp1(n, sum+1, memo) + dp1(n, sum+2, memo);
-        return memo[sum];
-    } 
-
-    // bottom-up (작은문제->큰문제, dp table+for)
-    public int dp(int n) {
-        if(n <= 2) return n;
-
-        int[] dp = new int[n+1];
-        dp[1] = 1;
-        dp[2] = 2;
-
-        for(int i=3; i<=n; i++) {
-            dp[i] = dp[i-1] + dp[i-2];
+        if(m[n] != -1) {
+            return m[n];
         }
 
-        return dp[n];
+        m[n] = dfs(n-1, m) + dfs(n-2, m);
+        return m[n];
     }
 }
