ba2a85bdfdd5a401b1921b45e718a2c23d9ab9d9
uz2ni <uzini_@likelion.org>
2026-08-02 19:26:59 +0900

    Time: 906 ms (5.01%), Space: 46.7 MB (10.15%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,42 +1,50 @@
 class Solution {
     public int[] dx = {-1,1,0,0};
     public int[] dy = {0,0,-1,1};
-
     public boolean exist(char[][] board, String word) {
-        // board 에서 이어지는 word가 존재하는지 여부 리턴
-        // dfs backtraking
-        // word 순서 동일해야함
+        // board에서 word 만들 수 있는지 반환
+        // visited true/false 처리해야 중복 위치 안감
+
         boolean[][] visited = new boolean[board.length][board[0].length];
+        
         for(int i=0; i<board.length; i++) {
             for(int j=0; j<board[0].length; j++) {
-                visited[i][j] = true; // 시작칸 방문
+                visited[i][j] = true;
                 if(dfs(board, word, String.valueOf(board[i][j]), i, j, visited)) {
                     return true;
                 }
-                visited[i][j] = false; // // 시작칸 복원
+                visited[i][j] = false;
             }
         }
-        
+
         return false;
     }
 
-    public boolean dfs(char[][] board, String target, String str, int x, int y, boolean[][] visited) {
-        if(target.equals(str)) return true;
-        if(!target.startsWith(str)) return false;
-
+    public boolean dfs(char[][] board, String word, String str, int i, int j, boolean[][] visited) {
         int r = board.length;
         int c = board[0].length;
-        for(int i=0; i<4; i++) {
-            int nx = x+dx[i];
-            int ny = y+dy[i];
-            if(nx < 0 || nx >= r || ny < 0 || ny >= c) continue;
+        
+        if(str.equals(word)) {
+            return true;
+        }
+        if(!word.startsWith(str)) {
+            return false;
+        }
+        if(str.length() > word.length()) {
+            return false;
+        }
+        
+        for(int k=0; k<4; k++) {
+            int nx = dx[k]+i;
+            int ny = dy[k]+j;
+            if(nx < 0 || ny < 0 || nx >= r || ny >= c) continue;
             if(visited[nx][ny]) continue;
-
-            visited[nx][ny] = true; // 이동칸 방문
-            if(dfs(board,target, str+board[nx][ny], nx, ny, visited)) {
+            
+            visited[nx][ny] = true;
+            if(dfs(board, word, str+board[nx][ny], nx, ny, visited)) {
                 return true;
             }
-            visited[nx][ny] = false; // 이동칸 복원
+            visited[nx][ny] = false;
         }
 
         return false;
