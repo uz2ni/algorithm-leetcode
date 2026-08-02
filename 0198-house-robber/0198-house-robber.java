de57ac8cf9be0b838d0fb046adbbb26fa9d6ecb8
uz2ni <uzini_@likelion.org>
2026-08-02 17:33:29 +0900

    Time: 0 ms (100%), Space: 42.8 MB (38.03%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int rob(int[] nums) {
+    public int rob1(int[] nums) {
         int k = nums.length-1;
         if(k == 0) return nums[0];
         if(k == 1) {
@@ -15,5 +15,28 @@ class Solution {
         }
 
         return dp[k];
+    }
+
+    public int rob(int[] nums) {
+        int k = nums.length-1;
+        if(k == 0) return nums[0];
+        if(k == 1) {
+            return Math.max(nums[0],nums[1]);
+        }
+        int[] m = new int[nums.length];
+        Arrays.fill(m,-1);
+        m[0] = nums[0];
+        m[1] = Math.max(nums[0],nums[1]);
+
+        return dfs(nums, nums.length-1, m);
+    }
+
+    public int dfs(int[] nums, int k, int[] m) {
+        if(m[k] != -1) {
+            return m[k];
+        }
+
+        m[k] = Math.max(dfs(nums, k-1, m), dfs(nums, k-2, m)+nums[k]);
+        return m[k];
     }
 }
