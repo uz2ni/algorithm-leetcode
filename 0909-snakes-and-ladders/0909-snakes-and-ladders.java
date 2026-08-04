43f7954e2f2f9e7310f5e7617aa5bf91140455ef
uz2ni <uzini_@likelion.org>
2026-08-04 15:27:43 +0900

    Time: 5 ms (83.94%), Space: 46.6 MB (62.8%) - LeetHub

--- 0909-snakes-and-ladders/0909-snakes-and-ladders.java
+++ 0909-snakes-and-ladders/0909-snakes-and-ladders.java
@@ -6,46 +6,35 @@ class Solution {
 
     }
     public int bfs(int[][] board, int n, boolean[][] visited) {
+
+        if(n == 1) return 0;
+
         Queue<int[]> q = new ArrayDeque<>(); // [num,cnt]
         q.offer(new int[]{1,0}); // 시작점
         visited[n-1][0] = true;
 
         while(!q.isEmpty()) {
             int[] cur = q.poll();
-            System.out.println("q 꺼냄 num: " + cur[0] + ", cnt: " + cur[1]);            
-
-            // basecase
-            if(cur[0] == n * n) {
-                return cur[1];
-            }
             
             for(int i=1; i<=6; i++) { // 주사위 경우의 수 6
-                int nxtNum = cur[0]+i;
-                int nxtCnt = cur[1]+1;
-                int[] nxtCord = numToXy(n, nxtNum);
+                int nxtNum = cur[0] + i;
+                int nxtCnt = cur[1] + 1;
 
-                // 이동 위치가 범위 벗어나면 skip
-                if(nxtCord[0] < 0 || nxtCord[1] < 0 || nxtCord[0] >= n || nxtCord[1] >= n) continue;
+                if (nxtNum > n * n) continue;
 
-                // if -1 이고 미방문이라면, 큐 삽입/방문처리
-                if((board[nxtCord[0]][nxtCord[1]] == -1)
-                    && !visited[nxtCord[0]][nxtCord[1]]) {
+                // 뱀/사다리 있으면 먼저 목적지로 치환 (여기서 최종 도착 번호 확정)
+                int[] cord = numToXy(n, nxtNum);
+                if (board[cord[0]][cord[1]] != -1) {
+                    nxtNum = board[cord[0]][cord[1]];
+                    cord = numToXy(n, nxtNum);
+                }
 
-                    System.out.println("q 삽입 nxtNum: " + nxtNum + ", nxtCnt: " + nxtCnt);                    
+                // 이제부터는 nxtNum이 "최종 도착 번호" - 체크/삽입 한 곳에서만
+                if (nxtNum == n * n) return nxtCnt;
+
+                if (!visited[cord[0]][cord[1]]) {
+                    visited[cord[0]][cord[1]] = true;
                     q.offer(new int[]{nxtNum, nxtCnt});
-                    visited[nxtCord[0]][nxtCord[1]] = true;
-
-                }else if((board[nxtCord[0]][nxtCord[1]] != -1)) {
-                    // 이동 번호의 위치 파악
-                    nxtNum = board[nxtCord[0]][nxtCord[1]];
-                    nxtCord = numToXy(n, nxtNum);
-                    
-                    // 이동 번호 위치 미방문이라면 큐 삽입/방문 처리
-                    if(!visited[nxtCord[0]][nxtCord[1]]) {
-                        System.out.println("q 삽입 nxtNum: " + nxtNum + ", nxtCnt: " + nxtCnt);                        
-                        q.offer(new int[]{nxtNum, nxtCnt});
-                        visited[nxtCord[0]][nxtCord[1]] = true;                        
-                    }
                 }
             }
         }
