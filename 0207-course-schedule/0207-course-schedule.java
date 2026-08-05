7362bf7e317f35194554f4bd1c93d8db2c11d5f9
uz2ni <uzini_@likelion.org>
2026-08-05 15:42:19 +0900

    Time: 4 ms (91.31%), Space: 47.2 MB (34.02%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,37 +1,58 @@
 class Solution {
-    public boolean canFinish(int numCourses, int[][] prerequisites) {
-        // 인접 리스트로 변경
-        List<List<Integer>> adjList = new ArrayList<>();
-        for(int i=0; i<numCourses; i++) {
-                adjList.add(new ArrayList<>());
-        }
-        for(int[] pre : prerequisites) {
-            adjList.get(pre[0]).add(pre[1]); // 선수과목들을 담아서 역방향으로 탐색
+    // 풀이 실패
+    public boolean canFinish1(int numCourses, int[][] prerequisites) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int[] p : prerequisites) {
+            if(map.isEmpty()) {
+                map.put(p[1],p[0]);
+                continue;
+            }
+            if(map.containsKey(p[0])) {
+                return false;
+            }
+            map.put(p[1],p[0]);
         }
         
-        int[] visited = new int[numCourses]; // 0:미방문 1:방문중 2:방문완료
+        return true;
+    }
+
+    // 풀이1 : DFS 그래프 사이클 판별
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        // graph 인접리스트
+        List<List<Integer>> graph = new ArrayList<>();
         for(int i=0; i<numCourses; i++) {
-            if(visited[i] == 0) {
-                if(hasCycle(i, adjList, visited)) {
-                    return false; // 사이클 발견 시 return
+            graph.add(new ArrayList<>());
+        } 
+        for(int i=0; i<prerequisites.length; i++) {
+            graph.get(prerequisites[i][1]).add(prerequisites[i][0]);
+        }
+        
+        int[] state = new int[numCourses]; // 0 미방문, 1 방문중, 2 완료
+
+        // 모든 노드를 시작으로 순회하며 사이클 체크하는 이유 : 그래프 하나로 연결 아닐수도 있어서
+        for(int i=0; i<numCourses; i++) { 
+            if(state[i] == 0) {
+                if(hasCycle(graph, state, i)) {
+                    return false;
                 }
             }
         }
+
         return true;
     }
-    public boolean hasCycle(int i, List<List<Integer>> adjList, int[] visited) {
-        // basecase
-        if(visited[i] == 1) return true; // 방문중 노드 다시 만난거라 사이클 존재
-        if(visited[i] == 2) return false; // 이미 안전한 노드라 사이클없이 끝냄
 
-        visited[i] = 1;
-        // recursive
-        for (int neighbor : adjList.get(i)) {
-            if(hasCycle(neighbor, adjList, visited)) {
+    public boolean hasCycle(List<List<Integer>> graph, int[] state, int node) {
+        state[node] = 1; // 방문중 표시
+        for(int nxt : graph.get(node)) {
+            if(state[nxt] == 1) { // 방문중인 노드 또 만남 -> 사이클
+                return true;
+            }
+            if(state[nxt] == 0 && hasCycle(graph, state, nxt)) { // 미방문이면 다음 노드부터 사이클 있는지 판별
                 return true;
             }
         }
-        visited[i] = 2;
+
+        state[node] = 2; // 노드 방문완료. 안전함 확정
         return false;
     }
 }
