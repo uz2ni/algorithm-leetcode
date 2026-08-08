5c1f57996989d1cc8139f5b69df5314ea1269c79
uz2ni <uzini_@likelion.org>
2026-08-08 23:53:35 +0900

    Time: 0 ms (100%), Space: 46.3 MB (84.74%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -2,15 +2,12 @@ class Solution {
     public int maxProfit(int[] prices) {
         // minPrice = 높->낮 이면 업데이트
         // minPrice = 낮->높 이면 현재-전 이익 add
-        int minPrice = prices[0];
+
         int maxProfit = 0;
         for(int i=1; i<prices.length; i++) {
-            if(minPrice < prices[i]) {
+            if(prices[i] > prices[i-1]) {
                 maxProfit += (prices[i]-prices[i-1]);
-                minPrice = prices[i];
-            }else {
-                minPrice = prices[i];
-            }
+            }            
         }
         return maxProfit;
     }
