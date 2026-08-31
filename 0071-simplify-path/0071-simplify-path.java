29c6159bbaa73d2514a8f1e88d9af72ac667fe37
uz2ni <uzini_@likelion.org>
2026-08-31 16:55:56 +0900

    Time: 3 ms (99.29%), Space: 45.4 MB (18.42%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.java
+++ 0071-simplify-path/0071-simplify-path.java
@@ -21,4 +21,34 @@ class Solution {
         return sb.toString().length() == 0 ? "/" : sb.toString();
         
     }
+
+    public static String simplifyPath2(String path) {
+        String[] splitStr = path.split("/");
+        Deque<String> stack = new ArrayDeque<>();
+
+        for(String s : splitStr) {
+            if(s.equals("..")) {
+                if(!stack.isEmpty()) {
+                    stack.pop();
+                }
+            }else if(s.equals(".") || s.equals("")) continue;
+            else {
+                stack.push(s);
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while(!stack.isEmpty()) {
+            // O(N^2) : 앞에 끼워넣으려면 n번 밀고, n개 반복
+            // sb.insert(0, "/"+stack.pop());
+
+            // O(N) : dir 문자열도 뒤집어서 이어붙임, 슬래시도 우측에 붙임
+            String dir = stack.pop();
+            sb.append(new StringBuilder(dir).reverse()).append("/");
+        }
+        // 마지막에 정방향으로 전체 뒤집기
+        sb.reverse();
+
+        return sb.toString().isEmpty() ? "/" : sb.toString();
+    }    
 }
