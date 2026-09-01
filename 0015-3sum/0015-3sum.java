38bbf536972d49a350a3fa5d03adb1491f838379
uz2ni <uzini_@likelion.org>
2026-09-01 16:04:21 +0900

    Time: 26 ms (99.66%), Space: 58.8 MB (90.99%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,38 +1,45 @@
 class Solution {
-    // 코드 이해 잘 안됨..
-    
+    // set/map 방식도 해보기
+
+    // 투포인터 방식
     public List<List<Integer>> threeSum(int[] nums) {
-        // 3개 골랐을 때 합이 0이 되는 조합 반환
-        Arrays.sort(nums);                          // ① 정렬 필수
-        List<List<Integer>> result = new ArrayList<>();
 
-        for (int i = 0; i < nums.length - 2; i++) {
-            // ② 고정값 중복 건너뛰기
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
+        List<List<Integer>> answer = new ArrayList<>();
 
-            // ③ 가지치기: 최솟값이 양수면 세 수 합은 절대 0 불가
-            if (nums[i] > 0) break;
+        Arrays.sort(nums);
 
-            int left = i + 1, right = nums.length - 1;
+        for(int i=0; i<nums.length-2; i++) { // length-2인 이유 : i위치 뒤에 2개를 골라야해서 i가 최대로 갈 수 있는 위치임
+
+            if(nums[i]>0) continue; // 첫선택값이 양수이면 뒤는 더 큰 수 뿐이라 합산 0 나올 수 없음
+            if(i>0 && nums[i] == nums[i-1]) continue; // 이전 위치와 값이 같다면 케이스가 중복이라 skip (i>0인 이유 : i-1 가 최소 0이어야해서)
+
             int target = -nums[i];
+            int left = i+1, right = nums.length-1;
 
-            while (left < right) {
+            while(left < right) {
+
                 int sum = nums[left] + nums[right];
 
-                if (sum == target) {
-                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
-                    // ④ 두 포인터 각각의 중복 건너뛰기
-                    while (left < right && nums[left] == nums[left + 1]) left++;
-                    while (left < right && nums[right] == nums[right - 1]) right--;
+                if(target == sum)  {
+
+                    answer.add(Arrays.asList(nums[i], nums[left], nums[right]));
                     left++;
                     right--;
-                } else if (sum < target) {
-                    left++;                         // 합이 작다 → 큰 값 쪽으로
-                } else {
-                    right--;                        // 합이 크다 → 작은 값 쪽으로
+
+                    // 중복 케이스 skip
+                    while(left<right && nums[left] == nums[left-1]) left++;
+                    while(left<right && nums[right] == nums[right+1]) right--;
+
+
+                // 아래 조건에서는 정답에 영향 없어서 중복 skip 없어도 됨
+                }else if(target > sum) {
+                    left++;
+                }else {
+                    right--;
                 }
             }
         }
-        return result;        
+
+        return answer;       
     }
 }
